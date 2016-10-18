5aadc81557a63ca87a6a118901561a81d1e9ef2d
Richard <richard.olney@mavs.uta.edu>
2016-10-18 00:20:51 -0500

    Final corrections to 7

--- 7-SortbyQuicksort/src/SortbyQuicksort.java
+++ 7-SortbyQuicksort/src/SortbyQuicksort.java
@@ -2,7 +2,8 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class SortbyQuicksort {
-
+	private static int array[];
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		try(Scanner scan = new Scanner(System.in)){
@@ -14,70 +15,39 @@ public class SortbyQuicksort {
 				int number = scan.nextInt();
 				unsorted[i] = number;
 			}
+			array = unsorted;
 			int first = 0;
 			int last = len - 1;
-			unsorted = pivotMove(unsorted);
-			int[] sorted = quickSort(unsorted,first,last);
+			SortbyQuicksort.quickSort(first, last);
 			System.out.println("The final result is:");
-			System.out.println(Arrays.toString(sorted));
+			System.out.println(Arrays.toString(array));
 		}
 	}
-	
-	public static int[] pivotMove(int[] original){//pick number from center of source array and move to pivot, so that if it is already sorted, will not degrade to least efficiency
-		int len = original.length;
-		int x = original[0];
-		original[0] = original[len/2];
-		original[len/2] = x;
-		return original;
-	}
-	
-	public static int[] quickSort(int[] unsorted, int first, int last){
-		int length = unsorted.length;
-		int left=first+1;
-		int right=last;
+	private static void quickSort(int left,int right){
+		int i = left;
+		int j = right;
 		int k = 0;
-		int pivot = first;
-		int[] blank = new int[length-1];
-		while(left<right){
-			if(unsorted[left]<unsorted[pivot])left++;
-			if(unsorted[right]>unsorted[pivot])right--;
-			if(unsorted[left]>unsorted[pivot]&&unsorted[right]<unsorted[pivot]){
-				k=unsorted[left];
-				unsorted[left]=unsorted[right];
-				unsorted[right]=k;
+		int pivot = array[i+(j-i)/2];
+		while (i<=j){
+			while (array[i]<pivot){
+				i++;
+			}
+			while (array[j]>pivot){
+				j--;
+			}
+			if (i<=j){
+				k=array[i];
+				array[i]=array[j];
+				array[j]=k;
+				i++;
+				j--;
 			}
 		}
-		if(right<=left){
-			k=unsorted[pivot];
-			unsorted[pivot]=unsorted[right];
-			unsorted[right]=k;
+		if (left < j){
+			quickSort(left,j);
 		}
-		if (length>3){
-			int Llen= right+1;
-			if (Llen>1){
-				int[] Larr=new int[Llen];
-				for(int j=0;j<Llen;j++){
-					Larr[j]=unsorted[j];
-				}
-				Larr = pivotMove(Larr);
-				Larr = quickSort(Larr,0,Llen-1);
-				for(int j=0;j<Llen;j++){
-					blank[j]=Larr[j];
-				}
-			}
-			int Rlen= last-right;
-			if (Rlen>1){
-				int[] Rarr=new int[Rlen];
-				for(int l=right+1;l<Rlen;l++){
-					Rarr[l]=unsorted[l];
-				}
-				Rarr = pivotMove(Rarr);
-				Rarr = quickSort(Rarr,0,Rlen-1);
-				for(int s=right+1;s<last;s++){
-					blank[s]=Rarr[s];
-				}
-			}
+		if (i < right){
+			quickSort(i,right);
 		}
-	return blank;
 	}
 }
